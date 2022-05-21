3bdf43c6177eade25aeaaf6312c974f3c5292104
eddy <649807433@qq.com>
2022-05-21 22:56:50 +0800

    add mircro service framework

--- MircroService/src/main/java/com/hzh/mircroservice/MircroServiceApplication.java
+++ MircroService/src/main/java/com/hzh/mircroservice/MircroServiceApplication.java
@@ -2,8 +2,10 @@ package com.hzh.mircroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MircroServiceApplication {
 
     public static void main(String[] args) {

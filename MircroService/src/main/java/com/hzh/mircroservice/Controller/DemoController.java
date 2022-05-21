9a6c3a6c4530dc7bc5a36c7c77141e991a0ec15b
eddy <649807433@qq.com>
2022-05-22 03:05:09 +0800

    mysql code

--- MircroService/src/main/java/com/hzh/mircroservice/Controller/DemoController.java
+++ MircroService/src/main/java/com/hzh/mircroservice/Controller/DemoController.java
@@ -1,14 +1,41 @@
 package com.hzh.mircroservice.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
+    @Autowired
+    DataSource dataSource;
+
     @GetMapping("test")
     public String testDemo(){
         return "test successful";
     }
+
+    @GetMapping("query")
+    public String testSelect() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        String sql = "select * from test";
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        ResultSet resultSet = pstmt.executeQuery();
+        System.out.println(resultSet);
+        while(resultSet.next()){
+            System.out.println(resultSet.getRow());
+        }
+        pstmt.close();
+        connection.close();
+        return "success";
+    }
+
+
 }
--- MircroService/src/test/java/com/hzh/mircroservice/MircroServiceApplicationTests.java
+++ MircroService/src/test/java/com/hzh/mircroservice/MircroServiceApplicationTests.java
@@ -1,10 +1,39 @@
 package com.hzh.mircroservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class MircroServiceApplicationTests {
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void testSelectAll()throws SQLException {
+        Connection connection = dataSource.getConnection();
+        String sql = "select * from tb_brand";
+        PreparedStatement pstmt = connection.prepareStatement(sql);
+        ResultSet resultSet = pstmt.executeQuery();
+        System.out.println(resultSet);
+        List<Integer> ids = new ArrayList();
+        while (resultSet.next()){
+            int id = resultSet.getInt("id");
+            ids.add(id);
+            System.out.println(ids);
+        }
+        System.out.println(ids);
+        pstmt.close();
+        connection.close();
+    }
 
     @Test
     void contextLoads() {

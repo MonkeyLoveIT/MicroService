0a821429bf1dcce9ed794a3079f17d6c8690a887
eddy <649807433@qq.com>
2022-05-21 22:01:32 +0800

    my first commit

---
+++ MircroService/src/main/java/com/hzh/mircroservice/MircroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hzh.mircroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MircroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MircroServiceApplication.class, args);
+    }
+
+}
---
+++ MircroService/src/test/java/com/hzh/mircroservice/MircroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzh.mircroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MircroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
